68007f43cc0611082b0704e7acdea5ea6ae23c1e
DJamesV <djames.veenstra@gmail.com>
2026-09-17 21:45:51 -0400

    Programming tools added such as c compilers and cargo

--- home/packages/default.nix
+++ home/packages/default.nix
@@ -8,15 +8,29 @@ in {
       lib.mkEnableOption "Installs libreoffice suite";
     packages.chatApps = 
       lib.mkEnableOption "Installs chat apps such as Discord";
+    packages.programmingTools = lib.mkDefault true;
   };
  
   config = {
     packages.officeApps = lib.mkDefault true;
     packages.chatApps = lib.mkDefault true;
+    packages.programmingTools = lib.mkDefault true;
 
     home.packages = lib.mkMerge [
       (lib.mkIf cfg.officeApps [ pkgs.libreoffice ])
-      (lib.mkIf cfg.chatApps [ pkgs.discord pkgs.concord-tui ]) 
+      (lib.mkIf cfg.chatApps [ pkgs.discord pkgs.concord-tui ])
+      (lib.mkIf cfg.programmingTools (with pkgs; [
+        unzip
+        gzip
+        gnutar
+        nodejs
+        python3
+        cargo
+        gcc
+        gnumake
+        cmake
+        binutils        
+      ])
     ];
   };
 }
